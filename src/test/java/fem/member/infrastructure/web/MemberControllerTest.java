4fca805ea48ea89e0b379968a7d47583f26fd95f
lee <lsg0814k@gmail.com>
2024-04-01 17:16:03 +0900

    회원 인증 Controller test 코드 추가

---
+++ src/test/java/fem/member/infrastructure/web/MemberControllerTest.java
@@ -0,0 +1,50 @@
+package fem.member.infrastructure.web;
+
+import fem.member.application.port.MemberRepository;
+import fem.member.domain.Member;
+import fem.member.domain.vo.MemberStatus;
+import fem.member.domain.vo.UserRole;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static fem.member.domain.vo.MemberStatus.PENDING;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemberControllerTest {
+    @Autowired private MockMvc mockMvc;
+    @Autowired private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("id 와 certificationCode 를 통해 Status 값을 ACTIVE 로 변경할 수 있다.")
+    void change_active_status_by_id_and_certificationCode() throws Exception{
+        // given
+        Member member = Member.builder()
+                .loginId("slee@naver.com")
+                .nickname("lee")
+                .password("a123456")
+                .certificationCode("aaaa-aaaa-aaaa")
+                .role(UserRole.USER)
+                .status(PENDING)
+                .build();
+        memberRepository.save(member);
+
+        // when
+        mockMvc.perform(get("/api/members/1/verify/aaaa-aaaa-aaaa"))
+                .andDo(print())
+                .andExpect(status().isFound());
+
+        // then
+        Member result = memberRepository.getById(1L);
+        assertThat(result.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+}
